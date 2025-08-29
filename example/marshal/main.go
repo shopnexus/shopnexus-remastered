76af498625e09d2481996c2b8d1d7b990f587ad2
Đậu Khoa <khoakomlem@gmail.com>
2025-08-29 11:57:34 +0700

    add examples

--- example/marshal/main.go
+++ example/marshal/main.go
@@ -7,6 +7,11 @@ import (
 	sharedmodel "shopnexus-remastered/internal/module/shared/model"
 )
 
+type CommonResponse struct {
+	Data  any               `json:"data,omitempty"`
+	Error sharedmodel.Error `json:"error,omitempty"`
+}
+
 type MyInt int
 
 func (m MyInt) MarshalJSON() ([]byte, error) {
@@ -17,4 +22,9 @@ func main() {
 	var x sharedmodel.Concurrency = 4212312312123123
 	data, _ := json.Marshal(x)
 	fmt.Println(string(data)) // "custom:42"
+
+	data, _ = json.Marshal(CommonResponse{
+		Data: MyInt(123),
+	})
+	fmt.Println(string(data)) // {"data":"custom:123"
 }
