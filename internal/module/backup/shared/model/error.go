e22ec75fa41f004a6801d2c0aeb93af8af9f0bc7
Đậu Khoa <khoakomlem@gmail.com>
2025-08-29 12:00:47 +0700

    add sharedmodel error

---
+++ internal/module/backup/shared/model/error.go
@@ -0,0 +1,30 @@
+package sharedmodel
+
+type ErrorWithCode interface {
+	Error() string
+	Code() string
+}
+
+type Error struct {
+	ErrCode string `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e Error) Error() string {
+	return e.Message
+}
+
+func (e Error) Code() string {
+	return e.ErrCode
+}
+
+func (e Error) IsEmpty() bool {
+	return e.ErrCode == "" && e.Message == ""
+}
+
+func NewError(code, message string) Error {
+	return Error{
+		ErrCode: code,
+		Message: message,
+	}
+}
