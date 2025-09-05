86669d8a51c3aab67ce4978fdd5b03908d64a897
Đậu Khoa <khoakomlem@gmail.com>
2025-09-05 11:34:00 +0700

    init promotion module

---
+++ internal/module/promotion/fx.go
@@ -0,0 +1,16 @@
+package promotion
+
+import (
+	promotionbiz "shopnexus-remastered/internal/module/promotion/biz"
+	promotionecho "shopnexus-remastered/internal/module/promotion/transport/echo"
+
+	"go.uber.org/fx"
+)
+
+// Module provides the promotion module dependencies
+var Module = fx.Module("promotion",
+	fx.Provide(
+		promotionbiz.NewCatalogBiz,
+		promotionecho.NewHandler,
+	),
+)
