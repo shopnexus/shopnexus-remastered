8b9f97ed5bc3c31821912202462042da41af29a5
Đậu Khoa <khoakomlem@gmail.com>
2025-09-05 11:34:44 +0700

    init system module

---
+++ internal/module/system/fx.go
@@ -0,0 +1,16 @@
+package system
+
+import (
+	systembiz "shopnexus-remastered/internal/module/system/biz"
+	systemecho "shopnexus-remastered/internal/module/system/transport/echo"
+
+	"go.uber.org/fx"
+)
+
+// Module provides the system module dependencies
+var Module = fx.Module("system",
+	fx.Provide(
+		systembiz.NewSystemBiz,
+		systemecho.NewHandler,
+	),
+)
